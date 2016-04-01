6f5ae5c1f124f3ffec07b4182af0be71af347b3e
root <root@localhost.localdomain>
2016-04-01 10:32:12 +0200

    Init of LFSR

--- src/LFSR.java
+++ src/LFSR.java
@@ -1,4 +1,28 @@
+import java.util.BitSet;
 
+/**
+ * @author Kenzo HOSOMI
+ * L'algorithme suivie : https://youtu.be/LgZAI3DdUA4
+ */
 public class LFSR {
+	int VI = 54;
+	
+	int LFSR1 = 0x3FFFFF;
+	int LFSR2 = 0x3FFFFF;
+	int LFSR3 = 0x3FFFFF;
 
+	int mask_7 = 0x40;
+	int mask_8 = 0x80;
+	int mask_10 = 0xA0;
+	int mask_13 = 0x1000;
+	int mask_16 = 0x10000;
+	int mask_17 = 0x20000;
+	int mask_18 = 0x40000;
+	int mask_20 = 0x80000;
+	int mask_21 = 0x100000;
+	int mask_22 = 0x200000;
+
+	
+	
+	// xor in java: ^
 }
