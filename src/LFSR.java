ba7b544885e32561d9868df53f566d05d2894f51
root <root@localhost.localdomain>
2016-04-01 11:27:47 +0200

    Add basic function for LFSR (xor and shift)

--- src/LFSR.java
+++ src/LFSR.java
@@ -1,12 +1,37 @@
-import java.util.BitSet;
-
 /**
- * @author Kenzo HOSOMI
- * L'algorithme suivie : https://youtu.be/LgZAI3DdUA4
+ * @author Kenzo HOSOMI L'algorithme suivie : https://youtu.be/LgZAI3DdUA4
  */
 public class LFSR {
+	public LFSR(int iv) {
+		
+		/*TODO:
+			Chiffrer un string puis un fichier
+			
+		*/
+		super();
+		
+		this.VI = iv;
+		this.LFSR1 = 0x3FFFFF;
+		this.LFSR2 = 0x3FFFFF;
+		this.LFSR3 = 0x3FFFFF;
+		this.mask_7 = 0x40;
+		this.mask_8 = 0x80;
+		this.mask_10 = 0xA0;
+		this.mask_13 = 0x1000;
+		this.mask_16 = 0x10000;
+		this.mask_17 = 0x20000;
+		this.mask_18 = 0x40000;
+		this.mask_20 = 0x80000;
+		this.mask_21 = 0x100000;
+		this.mask_22 = 0x200000;
+		this.and_19 = 0xFFFFF;
+		this.and_22 = 0x7FFFFF;
+		this.and_23 = 0xFFFFFF;
+	}
+
 	int VI = 54;
-	
+
+	/* Initialisation à 1*/
 	int LFSR1 = 0x3FFFFF;
 	int LFSR2 = 0x3FFFFF;
 	int LFSR3 = 0x3FFFFF;
@@ -21,8 +46,53 @@ public class LFSR {
 	int mask_20 = 0x80000;
 	int mask_21 = 0x100000;
 	int mask_22 = 0x200000;
+	
+	int and_19 = 0xFFFFF;
+	int and_22 = 0x7FFFFF;
+	int and_23 = 0xFFFFFF;
+	
 
+	/**
+	 * XOR pour la premiere LFSR
+	 * @param LFSR1
+	 * @return Dernier bit de l'opération
+	 */
+	public int xor_LFSR1(int LFSR1) {
+		return ((LFSR1 & mask_18)>>18) ^ ((LFSR1 & mask_17)>>17) ^ ((LFSR1 & mask_16)>>16)
+				^ ((LFSR1 & mask_13)>>13);
+	}
 	
+	/**
+	 * XOR pour la seconde LFSR
+	 * @param LFSR2
+	 * @return Dernier bit de l'opération
+	 */
+	public int xor_LFSR2(int LFSR2) {
+		return ((LFSR2 & mask_21)>>21) ^ ((LFSR2 & mask_20)>>20);
+	}
 	
-	// xor in java: ^
+	/**
+	 * XOR pour la troisieme LFSR
+	 * @param LFSR3
+	 * @return Dernier bit de l'opération
+	 */
+	public int xor_LFSR3(int LFSR3) {
+		return ((LFSR3 & mask_22)>>22) ^ ((LFSR3 & mask_21)>>21) ^ ((LFSR3 & mask_20)>>20)
+				^ ((LFSR3 & mask_17)>>17);
+	}
+	
+	/**
+	 * Décalage à gauche et ajout du dernier bit
+	 * @param LFSR
+	 * @param bit
+	 * @param and_mask
+	 * @return
+	 */
+	public int shift_LFSR(int LFSR, int bit, int and_mask){
+		LFSR = LFSR << 1;
+		LFSR = bit;		
+		LFSR = LFSR & and_mask;
+		
+		return LFSR;
+	}
 }
--- src/TpCrypto.java
+++ src/TpCrypto.java
@@ -1,9 +1,13 @@
-
 public class TpCrypto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int IV = 54;
+		LFSR lfsr;
+		
+		lfsr = new LFSR(IV);
+		
+		
 	}
 
 }
